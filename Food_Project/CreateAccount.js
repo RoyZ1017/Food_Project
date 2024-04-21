23fb0930f8fbd88c16efe2684e3e7d14dbb541b7
Praneeth Suryadevara <spraneeth42@gmail.com>
2024-04-21 17:13:11 -0400

    Add dropdown for time instead of text; verify opening is before closing

--- Food_Project/CreateAccount.js
+++ Food_Project/CreateAccount.js
@@ -1,58 +1,101 @@
 import React, { useState } from 'react';
-import { Button, StyleSheet, Text, View, Image, ScrollView, TextInput } from 'react-native';
+import { Button, StyleSheet, Text, View, ScrollView, TextInput, Picker } from 'react-native';
 
 const CreateAccountScreen = ({navigation}) => {
     const [name, setName] = useState('');
     const [location, setLocation] = useState('');
-    const [openingTime, setOpeningTime] = useState('');
-    const [closingTime, setClosingTime] = useState('');
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+
+    const [openingTimeHour, setOpeningTimeHour] = useState('');
+    const [openingTimeMinute, setOpeningTimeMinute] = useState('');
+    const [closingTimeHour, setClosingTimeHour] = useState('');
+    const [closingTimeMinute, setClosingTimeMinute] = useState('');
+    const hours = Array.from(Array(24).keys()).map(hour=> hour.toString().padStart(2, '0'));
+    const minutes = Array.from(Array(60).keys()).map(minute => minute.toString().padStart(2, '0'));
+
+    // So the user cannot add a closing time that is before the opening time
+    const validateTime = () => {
+        // Check if opening time is after closing time
+        if (parseInt(openingTimeHour) > parseInt(closingTimeHour) || 
+            (parseInt(openingTimeHour) == parseInt(closingTimeHour)) && (parseInt(openingTimeMinute) >= parseInt(closingTimeMinute))) {
+            alert('Opening time must be before closing time.');
+            return; // Exit if opening time is after closing time
+        }
+
+        // If works navigate to next page
+        navigation.navigate('Home');
+    };
+
     return(
         <View style={CreateAccountStyles.container}>
-            <Text style = {CreateAccountStyles.title}>Account Setup</Text> 
+            <Text style={CreateAccountStyles.title}>Account Setup</Text> 
             <TextInput
                 placeholder="Restaurant Name"
-                style = {CreateAccountStyles.textInput}
-                value = {name}
+                style={CreateAccountStyles.textInput}
+                value={name}
                 onChangeText={setName}
             />
             <TextInput
                 placeholder="Location"
-                style = {CreateAccountStyles.textInput}
-                value = {location}
+                style={CreateAccountStyles.textInput}
+                value={location}
                 onChangeText={setLocation}
             />
+            <View style={CreateAccountStyles.timePickerContainer}>
+                <Text>Opening Time:</Text>
+                <Picker style={CreateAccountStyles.timePicker}
+                    selectedValue={openingTimeHour}
+                    onValueChange={(itemValue) => setOpeningTimeHour(itemValue)}>
+                    {hours.map(hour => (
+                        <Picker.Item key={hour} label={hour.toString()} value={hour.toString()} />
+                    ))}
+                </Picker>
+                <Picker style={CreateAccountStyles.timePicker}
+                    selectedValue={openingTimeMinute}
+                    onValueChange={(itemValue) => setOpeningTimeMinute(itemValue)}>
+                    {minutes.map(minute => (
+                        <Picker.Item key={minute} label={minute.toString()} value={minute.toString()} />
+                    ))}
+                </Picker>
+            </View>
+            <View style={CreateAccountStyles.timePickerContainer}>
+                <Text>Closing Time:</Text>
+                <Picker style={CreateAccountStyles.timePicker}
+                    selectedValue={closingTimeHour}
+                    onValueChange={(itemValue) => setClosingTimeHour(itemValue)}>
+                    {hours.map(hour => (
+                        <Picker.Item key={hour} label={hour.toString()} value={hour.toString()} />
+                    ))}
+                </Picker>
+                <Picker style={CreateAccountStyles.timePicker}
+                    selectedValue={closingTimeMinute}
+                    onValueChange={(itemValue) => setClosingTimeMinute(itemValue)}>
+                    {minutes.map(minute => (
+                        <Picker.Item key={minute} label={minute.toString()} value={minute.toString()} />
+                    ))}
+                </Picker>
+            </View>
+
+
             <TextInput
-                placeholder="Opening Time"
-                style = {CreateAccountStyles.textInput}
-                value = {openingTime}
-                onChangeText={setOpeningTime}
-            />
-            <TextInput
-                placeholder="Closing Time"
-                style = {CreateAccountStyles.textInput}
-                value = {closingTime}
-                onChangeText={setClosingTime}
-            />
-            <TextInput
-                placeholder="Usernane"
-                style = {CreateAccountStyles.textInput}
-                value = {username}
+                placeholder="Username"
+                style={CreateAccountStyles.textInput}
+                value={username}
                 onChangeText={setUsername}
             />
             <TextInput
                 placeholder="Password"
-                style = {CreateAccountStyles.textInput}
+                style={CreateAccountStyles.textInput}
                 secureTextEntry={true}
-                value = {password}
+                value={password}
                 onChangeText={setPassword}
             />
             <View style={CreateAccountStyles.buttonContainer}>
                 <Button
-                    title = 'Create Account'
-                    onPress={() => navigation.navigate('Home')}
-                    disabled={!username || !password || !name || !location || !openingTime || !closingTime}
+                    title='Create Account'
+                    onPress={validateTime}
+                    disabled={!username || !password || !name || !location || !openingTimeHour || !openingTimeMinute || !closingTimeHour || !closingTimeMinute}
                 />
             </View>
         </View>
@@ -70,10 +113,19 @@ const CreateAccountStyles = StyleSheet.create({
         marginBottom: 20,
         backgroundColor: "#d3d3d3",
         borderRadius: 20,
-        height: "5%",
-        padding: 5,
+        height: 40,
+        padding: 10,
     },
-
+    timePickerContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 20,
+    },
+    timePicker: {
+        height: 40,
+        width: 80,
+        marginLeft: 15,
+    },
     buttonContainer: {
         margin: 5,
         width: "10%",
@@ -81,9 +133,8 @@ const CreateAccountStyles = StyleSheet.create({
     title: {
         fontSize: 26,
         fontWeight: 'bold',
-        marginBottom: "2%"
-      },
-
+        marginBottom: 20,
+    },
 });
 
-export default CreateAccountScreen
+export default CreateAccountScreen;
